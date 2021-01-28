7c2953429dec5fb9d758a49a2e7f73e7c8ea5f4a
Shrey Rai <kumarshrey.21099@gmail.com>
2021-01-28 21:35:02 +0530

    Redux devtools prod added

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const initialState={};
 const middleware = [thunk];
@@ -9,10 +10,11 @@ const middleware = [thunk];
 const store = createStore(
     rootReducer,
      initialState ,
-     compose(
+     composeWithDevTools(applyMiddleware(...middleware))
+     /*compose(
          applyMiddleware(...middleware),
          window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
-     )
+     )*/
      
 );
 
