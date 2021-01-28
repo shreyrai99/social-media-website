524c108a2084d9fc9bb561baaea5cd75f2fe4cb8
Shrey Rai <kumarshrey.21099@gmail.com>
2021-01-28 19:43:03 +0530

    Prepared for deployment

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const posts = require('./routes/api/posts');
 const profile = require('./routes/api/profile');
 const bodyParser =require('body-parser');
 const passport = require('passport');
+const path = require('path');
+
 // Body Parser Middleware**************
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -41,5 +43,16 @@ app.use('/api/users',users);
 app.use('/api/profile',profile);
 app.use('/api/posts',posts);
 //********************************* */
+
+//serve static assets if in production
+if(process.env.NODE_ENV === 'production')
+{
+    //use static folder
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 app.listen(port,()=> console.log(`....Server running on port ${port}....`));
